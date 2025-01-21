496ffb6ec8538c8d05db61e133e5c6aa4eca8ee9
Sandesh Baral <sandeshbaral99@gmail.com>
2025-01-21 16:34:07 +0545

    auth scafold installed

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,8 @@ Route::get('/blog/create',[BlogController::class, 'create'])->name('blog.create'
 Route::post('/blog',[BlogController::class,'store'])->name('blog.store');
 Route::get('/blog/{id}/edit',[BlogController::class,'edit'])->name('blog.edit');
 Route::put('/blog/{id}',[BlogController::class,'update'])->name('blog.update');
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
