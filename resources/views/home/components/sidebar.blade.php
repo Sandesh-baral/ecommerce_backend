5abb1010f5709146916a24879bdd17c9082f0785
Sandesh Baral <sandeshbaral99@gmail.com>
2025-01-23 13:02:08 +0545

    login middleware implementation started

---
+++ resources/views/home/components/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="w-64 bg-gray-800 text-white h-screen fixed">
+    <div class="p-4">
+        <h2 class="text-lg font-semibold">Admin Panel</h2>
+    </div>
+    <nav class="mt-6">
+        <a href="{{ route('dashboard') }}" class="block px-4 py-2 hover:bg-gray-700">Dashboard</a>
+        <a href="{{ route('users') }}" class="block px-4 py-2 hover:bg-gray-700">Users</a>
+        <a href="{{ route('blog') }}" class="block px-4 py-2 hover:bg-gray-700">Blog</a>
+        <a href="{{ route('category') }}" class="block px-4 py-2 hover:bg-gray-700">Category</a>
+        <a href="{{ route('settings') }}" class="block px-4 py-2 hover:bg-gray-700">Settings</a>
+    </nav>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Category;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
@@ -12,12 +13,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
+
+
 //Author
-Route::get('/author',[AuthorController::class,'index'])->name('author.index');
-Route::get('/author/create',[AuthorController::class,'create'])->name('author.create');
-Route::post('/author',[AuthorController::class,'store'])->name('author.store');
-Route::delete('/author/{id}',[AuthorController::class,'destroy'])->name('author.delete');
-Route::get('/author/show/{id}',[AuthorController::class,'show'])->name('author.show');
+Route::get('/author', [AuthorController::class, 'index'])->name('author.index');
+Route::get('/author/create', [AuthorController::class, 'create'])->name('author.create');
+Route::post('/author', [AuthorController::class, 'store'])->name('author.store');
+Route::delete('/author/{id}', [AuthorController::class, 'destroy'])->name('author.delete');
+Route::get('/author/show/{id}', [AuthorController::class, 'show'])->name('author.show');
 Route::put('/author/{id}', [AuthorController::class, 'update'])->name('author.update');
 Route::get('/author/{id}/edit', [AuthorController::class, 'edit'])->name('author.edit');
 
@@ -25,26 +31,44 @@ Route::get('/author/{id}/edit', [AuthorController::class, 'edit'])->name('author
 
 //Category
 
-Route::get('/category',[CategoryController::class,'index'])->name('category.index');
-Route::get('/category/create',[CategoryController::class,'create'])->name('category.create');
-Route::post('/category',[CategoryController::class,'store'])->name('category.store');
-Route::put('/category/{id}',[CategoryController::class,'update'])->name('category.update');
-Route::get('/category/{id}/edit',[CategoryController::class,'edit'])->name('category.edit');
+Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
+Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
+Route::get('/category/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
 
 
 //Blog
 
 
-Route::get('/blog',[BlogController::class,'index'])->name('blog.index');
-Route::get('/blog/create',[BlogController::class, 'create'])->name('blog.create');
-Route::post('/blog',[BlogController::class,'store'])->name('blog.store');
-Route::get('/blog/{id}/edit',[BlogController::class,'edit'])->name('blog.edit');
-Route::put('/blog/{id}',[BlogController::class,'update'])->name('blog.update');
+// Route::get('/blog',[BlogController::class,'index'])->name('blog.index');
+// Route::get('/blog/create',[BlogController::class, 'create'])->name('blog.create');
+// Route::post('/blog',[BlogController::class,'store'])->name('blog.store');
+// Route::get('/blog/{id}/edit',[BlogController::class,'edit'])->name('blog.edit');
+// Route::put('/blog/{id}',[BlogController::class,'update'])->name('blog.update');
+
+// Route::prefix('blog')
+//         ->middleware['user.auth']   // naming convention for middleware is small letters 
+//         ->name('blog.')
+//         ->group(function() {
+//             Route::get('/',[BlogController::class,'index'])->name('index');
+//             Route::get('/create',[BlogController::class, 'create'])->name('create');
+//             Route::post('/',[BlogController::class,'store'])->name('store');
+//             Route::get('/{id}/edit',[BlogController::class,'edit'])->name('edit');
+//             Route::put('/{id}',[BlogController::class,'update'])->name('update');
+//         });
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
+Route::prefix('blogs')
+    ->middleware(['auth']) // This correctly applies the middleware
+    ->name('blog.')
+    ->group(function () {
+        Route::get('/', [BlogController::class, 'index'])->name('index');
+        Route::get('/create', [BlogController::class, 'create'])->name('create');
+        Route::post('/', [BlogController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [BlogController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [BlogController::class, 'update'])->name('update');
+    });
 
 
 
